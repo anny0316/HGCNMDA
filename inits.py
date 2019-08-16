0ccc2ea4d17bb4c69ca1e356c5cf4d78963fd763
anny0316 <43496750+anny0316@users.noreply.github.com>
2019-08-16 16:42:11 +0800

    Add files via upload

---
+++ inits.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+import numpy as np
+
+
+def uniform(shape, scale=0.05, name=None):
+    """Uniform init."""
+    initial = tf.random_uniform(shape, minval=-scale, maxval=scale, dtype=tf.float32)
+    return tf.Variable(initial, name=name)
+
+
+def glorot(shape, lambda1, name=None):
+    """Glorot & Bengio (AISTATS 2010) init."""
+    init_range = np.sqrt(6.0/(shape[0]+shape[1]))
+    initial = tf.random_uniform(shape, minval=-init_range, maxval=init_range, dtype=tf.float32)
+    var = tf.Variable(initial, name=name)
+    tf.add_to_collection( 'losses', tf.contrib.layers.l2_regularizer(lambda1)(var) )
+    return var
+
+def zeros(shape, name=None):
+    """All zeros."""
+    initial = tf.zeros(shape, dtype=tf.float32)
+    return tf.Variable(initial, name=name)
+
+
+def ones(shape, name=None):
+    """All ones."""
+    initial = tf.ones(shape, dtype=tf.float32)
+    return tf.Variable(initial, name=name)
